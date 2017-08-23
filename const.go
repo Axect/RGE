1121b58b329a053c0c22b140320ae7c1c9045a81
Axect <edeftg@gmail.com>
2017-08-23 23:14:47 +0900

    Initial Commit

---
+++ const.go
@@ -0,0 +1,18 @@
+package rge
+
+// -----------------------
+//	Declare Constants
+// -----------------------
+
+const (
+	Mp       = 1.221 * 1E+19 // Plank Mass
+	MpR      = 2.4 * 1E+18   // Reduced Planck Mass
+	MW       = 80.385        // Mass of W
+	MZ       = 91.1876       // Mass of Z
+	MH       = 125.09        // Mass of Higgs
+	alphasMZ = 0.1182        // alphas(MZ)
+
+	// Running Constants (Precision & Number of Lists)
+	h    = 1E-04      // precision
+	Step = 1E+04 * 44 // Number of lists
+)
