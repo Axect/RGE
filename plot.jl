005a01bfa75d3f7c960389ebaa4c80646fc68d90
Axect <edeftg@gmail.com>
2017-08-24 18:19:53 +0900

    Fix Background

--- plot.jl
+++ plot.jl
@@ -21,21 +21,21 @@ function main()
     # Gauge Plot
     if "1" in choice
         df = DataFrame(t=repeat(t, outer=[4]), gauge=gauge, index=repeat(["yt", "g1", "g2", "g3"], inner=[length(t)]));
-        pl = plot(df, x=:t, y=:gauge, color=:index, Geom.line, Guide.title("Gauge"))
+        pl = plot(df, x=:t, y=:gauge, color=:index, Geom.line, Guide.title("Gauge"), Theme(background_color=color("white")))
         draw(SVG("Fig/Gauge_170_85_50.svg", 1000px, 600px), pl)
         println("Complete Gauge")
     end
 
     if "2" in choice
         dg = DataFrame(t=t, G=G, index=repeat(["G",], inner=[length(t)]));
-        pl2 = plot(dg, x=:t, y=:G, color=:index, Geom.line, Guide.title("G(t)"))
+        pl2 = plot(dg, x=:t, y=:G, color=:index, Geom.line, Guide.title("G(t)"), Theme(background_color=color("white")))
         draw(SVG("Fig/G_170_85_50.svg", 1000px, 600px), pl2)
         println("Complete G(t)")
     end
 
     if "3" in choice
         dh = DataFrame(t=t, λ=λ, index=repeat(["λ",], inner=[length(t)]));
-        pl3 = plot(dh, x=:t, y=:λ, color=:index, Geom.line, Guide.title("λ(t)"))
+        pl3 = plot(dh, x=:t, y=:λ, color=:index, Geom.line, Guide.title("λ(t)"), Theme(background_color=color("white")))
         draw(SVG("Fig/lambda_170_85_50.svg", 1000px, 600px), pl3)
         println("Complete λ(t)")
     end
