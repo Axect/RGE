9a2ab2d098158b3c079bba4fdf4741df75eef0a2
Axect <edeftg@gmail.com>
2017-09-02 20:47:40 +0900

    Remove Bay

--- type.go
+++ type.go
@@ -3,8 +3,5 @@ package RGE
 // Container contains coupling constamts
 type Container [Step]RGE
 
-// Bay is array of Container
-type Bay []Container
-
 // Gamma for Convenience
 type Gamma func(float64, float64) float64
